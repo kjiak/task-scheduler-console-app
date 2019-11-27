43d7c9e500dec91afa4ff6000bb755be20a91a91
kjiakai <kjiakai@gmail.com>
2019-11-27 21:14:41 +0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# task-scheduler-console-app
+
+### Credits
+https://www.c-sharpcorner.com/UploadFile/manas1/console-application-using-windows-scheduler/ <br>
---
+++ task-scheduler-console-app/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace task_scheduler_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunSchedule();
+        }
+        public static void RunSchedule()
+        {
+            string path = Path.GetFullPath("C:\\MyTest") + "\\" + DateTime.Now.ToString("MM_dd_yyyy_HH_mm") + "_Log.txt";
+            try
+            {
+                // Put your own logic which you want to perform on schedule basis
+
+                if (!File.Exists(path))
+                {
+                    File.Create(path);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log error and send email to Admin that schedule has failed
+                string errorLogPath = @"C:\MyTest.txt";
+                File.AppendAllText(errorLogPath, Environment.NewLine + ex.Message);
+            }
+        }
+    }
+}
